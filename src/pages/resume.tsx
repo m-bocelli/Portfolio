2796fa9c056b769de530d59e356350ebc7bf7dab
Mikey <michael.j.bocelli@gmail.com>
2024-03-05 18:06:25 -0500

    fix: resume path

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,7 +2,7 @@ import ResumeEmbed from '@/components/ResumeEmbed/ResumeEmbed';
 import FlexContainer from '../components/FlexContainer/FlexContainer';
 
 export default function Resume() {
-    const resumePath = '/Resume2_28.pdf';
+    const resumePath = '/Resume3_4.pdf';
 
     return (
         <FlexContainer>
