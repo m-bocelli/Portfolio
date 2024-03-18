e27055bc93dc4fecb742ec938a5a117a7af13742
Mikey <michael.j.bocelli@gmail.com>
2024-03-18 14:17:36 -0400

    change: updated resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,7 +2,7 @@ import ResumeEmbed from '@/components/ResumeEmbed/ResumeEmbed';
 import FlexContainer from '../components/FlexContainer/FlexContainer';
 
 export default function Resume() {
-    const resumePath = '/Resume3_4.pdf';
+    const resumePath = '/resume';
 
     return (
         <FlexContainer>
